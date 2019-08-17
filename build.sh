13fe200636a45d1c1c3f9b2dccd5bcb34cdfeb2e
Andrei Cristian Petcu <andrei@ceata.org>
2019-08-18 01:16:07 +0300

    version bump

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #build desktop
-zip tr.zip manifest.json scripts/* images/* _locales/*
+zip tr_desktop.zip manifest.json scripts/* images/* _locales/*
 
 #build android
 mv manifest.json manifest_desktop.json
 cp manifest_android.json manifest.json
-zip android_tr.zip manifest.json scripts/* images/* _locales/*
+zip tr_android.zip manifest.json scripts/* images/* _locales/*
 mv manifest_desktop.json manifest.json
