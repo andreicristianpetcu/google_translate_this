bcc2676f3e18c5ef11cbad1ed4fff020df2c12a3
Andrei Cristian Petcu <andrei@ceata.org>
2019-08-18 01:41:12 +0300

    build fixes and push new versions

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 zip tr_desktop.zip manifest.json scripts/* images/* _locales/**/*
 
 #build android
-mv manifest.json manifest_desktop.json
+cp manifest.json manifest_desktop.json
 cp manifest_android.json manifest.json
 zip tr_android.zip manifest.json scripts/* images/* _locales/**/*
-mv manifest_desktop.json manifest.json
+cp manifest_desktop.json manifest.json
+rm -rf manifest_desktop.json
