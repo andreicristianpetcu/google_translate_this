ff0f06102fa03f4c67334d42bace1275dd4b9101
Andrei Cristian Petcu <andrei@ceata.org>
2019-08-22 02:11:38 +0300

    generate updates for both platforms

--- build.sh
+++ build.sh
@@ -1,35 +1,47 @@
 version=$1
 
+generate_update_json() {
+  new_version=$1
+  browser_type=$2
+  update_file_name="updates.json"
+  addon_id="e34d5840-6b3b-49d8-92c2-9696798c4e2a"
+  build_fragment="fx"
+  if [ "$browser_type" == "android" ]; then
+    update_file_name="updates_android.json"
+    addon_id="aaaa5840-6b3b-49d8-92c2-9696798c4e2a"
+    build_fragment="an"
+  fi
+  sed -i "/$new_version/d" versions.txt
+  echo "{
+  \"addons\": {
+    \"{$addon_id}\": {
+      \"updates\": [" > "$update_file_name"
+
+  while IFS= read -r old_version; do
+    echo "        {
+          \"version\": \"$old_version\",
+          \"update_link\": \"https://github.com/andreicristianpetcu/google_translate_this/releases/download/v$old_version/google_translate_this_page-$old_version-$build_fragment.xpi\"
+        }," >>"$update_file_name"
+  done <versions.txt
+
+  printf "$new_version\n" >>versions.txt
+  echo "        {
+          \"version\": \"$new_version\",
+          \"update_link\": \"https://github.com/andreicristianpetcu/google_translate_this/releases/download/v$new_version/google_translate_this_page-$new_version-$build_fragment.xpi\"
+        }
+     ]
+    }
+  }
+}" >>"$update_file_name"
+}
+
 sed -i -E "s/\"version\":(.*)/\"version\": \"$version\",/" manifest.json
 sed -i -E "s/\"version\":(.*)/\"version\": \"$version\",/" manifest_android.json
 cat rm_template.md > README.md
 sed -i -E "s/NEW_VERSION/$version/" README.md
 
-sed -i "/$version/d" versions.txt
-
-echo '{
-  "addons": {
-    "{e34d5840-6b3b-49d8-92c2-9696798c4e2a}": {
-      "updates": [' > updates_android.json
-
-while IFS= read -r old_version
-do
-echo "        {
-          \"version\": \"$old_version\",
-          \"update_link\": \"https://github.com/andreicristianpetcu/google_translate_this/releases/download/v$old_version/google_translate_this_page-$old_version-an.xpi\"
-        }," >> updates_android.json 
-done < versions.txt
-
-printf "$version\n" >> versions.txt
-echo "        {
-          \"version\": \"$version\",
-          \"update_link\": \"https://github.com/andreicristianpetcu/google_translate_this/releases/download/v$version/google_translate_this_page-$version-an.xpi\"
-        }
-     ]
-    }
-  }
-}" >> updates_android.json
-
+generate_update_json "$version" "android"
+generate_update_json "$version" "desktop"
 
 #build desktop
 cat manifest.json | grep version
